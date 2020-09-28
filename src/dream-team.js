3aa96f94ea59ae3484cde1f6ce57f94cada4bf36
dzmitry.alfer <dzmitry.alfer@gmail.com>
2020-09-28 16:38:05 +0300

    Add dream team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,15 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (!members || !Array.isArray(members)) {
+    return false;
+  }
+  
+  return members
+    .filter(x => typeof x === 'string')
+    .map(x => x.trim())
+    .filter(x => x.length > 0)
+    .map(x => [0].toUpperCase())
+    .sort()
+    .join('');
 };
