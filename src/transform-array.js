733e8cdefb733b0188cd67593a4f3d7a75f65fa1
dzmitry.alfer <dzmitry.alfer@gmail.com>
2020-09-29 19:03:45 +0300

    Add transform array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,61 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {debugger;
+  if (!Array.isArray(arr)) {
+    throw new Error();
+  }
+
+  const result = [];
+  let i = 0;
+
+  let isPrevDiscarded = false;
+
+  while(i < arr.length){
+    const currentValue = arr[i];
+
+    if (typeof currentValue === 'string') {
+      switch(currentValue){
+        case '--discard-next':
+          i++;
+          isPrevDiscarded = true;
+          break;
+        
+        case '--discard-prev':
+          if(!isPrevDiscarded) {
+            result.pop();
+          }
+          isPrevDiscarded = false;
+
+          break;
+        
+        case '--double-next':
+          if(arr.length > i + 1) {
+            result.push(arr[i + 1]);
+          }
+          isPrevDiscarded = false;
+
+          break;
+        
+        case '--double-prev':
+          if(i - 1 >= 0 && !isPrevDiscarded) {
+            result.push(arr[i - 1]);
+          }
+          isPrevDiscarded = false;
+
+          break;
+        
+          default:
+            result.push(currentValue);
+            isPrevDiscarded = false;
+            break;
+      }
+    } else {
+      result.push(currentValue);
+      isPrevDiscarded = false;
+    }
+    
+    i++;
+  }
+
+  return result;
 };
